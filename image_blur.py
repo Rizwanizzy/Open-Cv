502b5eadd4dc7da42f4511ebb0d0b597185cd145
Mohammed Rizwan <mohammedrizwanizzy@gmail.com>
2022-11-17 20:06:15 +0530

    different blur methods

--- image_blur.py
+++ image_blur.py
@@ -1,9 +1,14 @@
 import cv2
 import numpy
-image=cv2.imread('img.jpg',1)
-matrix=numpy.ones((5,5),numpy.float32)/25
-new_image=cv2.filter2D(image,-5,matrix)
-cv2.imshow('original',image)
-cv2.imshow('blur image',new_image)
+
+image = cv2.imread('img.jpg', 1)
+# matrix=numpy.ones((5,5),numpy.float32)/25
+# new_image=cv2.filter2D(image,-5,matrix)
+blur = cv2.blur(image, (5, 5))
+gaussian_blur = cv2.GaussianBlur(image, (5, 5), 0)
+cv2.imshow('original', image)
+# cv2.imshow('blur image',new_image)
+cv2.imshow('blur image', blur)
+cv2.imshow('Gaussian image', gaussian_blur)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
